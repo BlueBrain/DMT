74110510542551170ac1b40bdc1d98f35f79e95e
Vishal Sood Muchu <vishal.sood@saphetor.com>
2018-07-09 17:13:35 +0200

    design a phenomenon with registeries

--- neuro_dmt/phenomenon/circuit/composition/__init__.py
+++ neuro_dmt/phenomenon/circuit/composition/__init__.py
@@ -1,6 +1,15 @@
 """Phenomena related to the composition of a brain region"""
 
+from dmt.phenomenon import Phenomenon
 from neuro_dmt.phenomenon.circuit import BrainRegionPhenomenon
+
+cell_density = Phenomenon(name = "cell density",
+                          description = """Number of cells in a unit volume
+                          of a brain region""")
+
+inhibitory_cell_ratio = Phenomenon(name = "inhibitory cell ratio",
+                                   description = """Ratio of inhibitory to
+                                   excitatory cells in a brain region""")
 
 
 class Composition(BrainRegionPhenomenon):
@@ -15,13 +24,12 @@ class CellDensity(Composition):
 
     name = "cell density"
 
-    
     description = """Number of cells in a unit volume of a brain region"""
 
 
 class InhibitoryCellRatio(Composition):
     """Ratio of inhibitory to excitatory cells in a brain region."""
 
-        name = "inhibitory cell ratio"
+    name = "inhibitory cell ratio"
 
-        description = "ratio of inhibitory to excitatory cells in a brain region."
+    description = "Ratio of inhibitory to excitatory cells in a brain region"
