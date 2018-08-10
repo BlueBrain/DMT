adb8f1184ce626b09ab2a4319afd7f640690c9c8
Vishal Sood <vishal.chandra.sood@gmail.com>
2018-08-10 17:42:13 +0200

    introduce a scratch directory to scratch ideas

---
+++ scratch/adapter_exptl.py
@@ -0,0 +1,70 @@
+"""We do not like the current way of specifying adapter methods."""
+
+from abc import ABC, abstractmethod
+import pandas as pd
+from dmt.aii import adaptermethod, implementation
+from dmt.validation.test_case import ValidationTestCase
+from dmt.vtk.author import Author
+
+class TestIntegerMethod(ValidationTestCase):
+    """An example showing how to write a ValidationTestCase.
+    """
+    author = Author(name="Vishal Sood",
+                    affiliation="EPFL",
+                    user_id=1)
+
+    class AdapterInterface:
+        """Specify all the methods you require from your adapter here. In order
+        to use this validation test case, the user will have to program an
+        adapter for their model. The adapter must satisfy all the requirements
+        specified in this interface.
+
+        Remember that all methods defined in the body of an interface will be
+        considered abstract --- only their __doc__ string will be kept.
+        Everything else in the body of these methods will be thrown away.
+        """
+        def get_addition(self, model, x, y):
+            """Add x and y.
+
+            Parameters
+            --------------------------------------------------------------------
+            x :: int
+            y :: int
+
+            Return
+            --------------------------------------------------------------------
+            int
+            """
+            pass
+
+        def get_subtraction(self, model, x, y):
+            """Subtract x and y.
+
+            Parameters
+            --------------------------------------------------------------------
+            x :: int
+            y :: int
+
+            Return
+            --------------------------------------------------------------------
+            int
+            """
+            pass
+
+    def __call__(self, model):
+        """"Makes this ValidationTestCase callable.
+        Each ValidationTestCase must implement a '__call__' method.
+
+        Parameters
+        ------------------------------------------------------------------------
+        model :: To be validated.
+        """
+        d = self.validation_data
+        addition_measurement\
+            = self.model_adapter.get_addition(model, d.x, d.y)
+        subtraction_measurement\
+            = self.model_adapter.get_addition(model, d.x, d.y)
+        return ('PASS' if (all(addition_measurement == d.z) and
+                           all(subtraction_measurement == d.w))
+                else 'FAIL')
+
