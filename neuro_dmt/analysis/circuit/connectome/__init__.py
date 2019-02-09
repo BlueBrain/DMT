3b505d19dfa11045e8139d47aa306bf1cd200406
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-02-09 21:59:12 +0100

    Intermittent stop
    When finished, this commit will
    1. Break connectome analyses into
       CellGroup baesd and Pathway based...
    2. Plot HeatMaps from Connectome Pathway Analysis

--- neuro_dmt/analysis/circuit/connectome/__init__.py
+++ neuro_dmt/analysis/circuit/connectome/__init__.py
@@ -6,8 +6,6 @@ from dmt.vtk.utils.descriptor\
     ,      document_fields
 from neuro_dmt.analysis.circuit\
     import BrainCircuitAnalysis
-from neuro_dmt.measurement.parameter\
-    import BrainCircuitConnectomeParameter
 from neuro_dmt.models.bluebrain.circuit.parameters\
     import Mtype\
 
@@ -15,27 +13,4 @@ from neuro_dmt.models.bluebrain.circuit.parameters\
 class ConnectomeAnalysis(
         BrainCircuitAnalysis):
     """Analyze a single connectome phenomenon."""
-
-    pathway_parameters=\
-        Field(
-            __name__ = "pathway_parameters",
-            __typecheck__ = Field.typecheck.collection(
-                BrainCircuitConnectomeParameter),
-            __doc__ = """A connectome phenomenon must be measured as a
-            function of either cell-type (for example mtype) or a
-            cell-type --> cell-type pathway. Most often we will use mtype
-            as cell-type.""")
-    cell_group_parameters=\
-        Field(
-            __name__ = "cell_group_parameters",
-            __typecheck__ = Field.typecheck.collection(
-                BrainCircuitConnectomeParameter),
-            __doc__ = """A connectome phenomenon must be measured as a
-            function of either cell-type (for example mtype) or a
-            cell-type --> cell-type pathway. Most often we will use mtype
-            as cell-type.""")
-    def __init__(self,
-            *args, **kwargs):
-        """Initialize Me"""
-        super().__init__(
-            *args, **kwargs)
+    pass
