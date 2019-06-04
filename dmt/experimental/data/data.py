027dfa00faf0a359f1d8f9c8735f5292c9f7a8cd
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-06-04 19:30:20 +0200

    Define metaclass DataClassConstructor
    DataClassConstructor establishes a pattern that
    we will abstract away into a base FieldMeta that
    will check that Fields defined in a derived metaclass
    have been defined in the class to be constructed by
    that metaclass.

--- dmt/experimental/data/data.py
+++ dmt/experimental/data/data.py
@@ -3,39 +3,107 @@ Prototype for Data.
 """
 from abc import ABCMeta
 import pandas as pd
-from dmt.v2.tk.field import Field, WithFields
+from dmt.v2.tk.journal import Logger
+from dmt.v2.tk.field import Field, WithFields, FieldMeta
 
-class Data(WithFields):
+
+def is_data_base_class(cls):
+    """???"""
+    return getattr(cls, "__data_base_class__", False)
+
+class DataClassConstructor(type):
     """
-    Well documented data.
-    Each attribute must be well documented, such that their documentation
-    can be assembled to produce documentation for the entire class.
+    A meta class to construct Data classes
     """
+
+    def __new__(mcs, name, bases, namespace):
+        """..."""
+        logger = Logger(client="DataClassConstructor", level="STUDY")
+        logger.study(
+            """Construct new class with
+            mcs {},
+            name {},
+            bases {}"""\
+            .format(
+                mcs, name, bases))
+        logger.study(
+            """namespace has data_base_class__? {}""".format(
+                namespace.get("__data_base_class__", False)))
+        if not namespace.get("__data_base_class__", False):
+            if "index" not in namespace:
+                logger.alert("""
+                Provide class attribute 'index':
+                A dict mapping the name of an index variable to its description.
+                The names will be used to name the dataframe index,
+                and the description to provide documentation.
+                """)
+                raise TypeError(
+                    "Missing class attribute 'index' in class definition of '{}'"\
+                    .format(name))
+            if "measurements" not in namespace:
+                logger.alert("""
+                Provide class attribute 'measurements':
+                A dict mapping the name of measurement variable to its description.
+                The names will be used to name the dataframe columns, 
+                and the description to provide documentation.
+                """)
+                raise TypeError(
+                    "Missing class attribute 'measurements' in class definition of '{}'"\
+                    .format(name))
+
+        return super().__new__(
+            mcs, name, bases, namespace)
+
+    def __init__(cls, name, bases, namespace):
+        """..."""
+        super().__init__(
+            name, bases, namespace)
+
+
+class Data(
+        WithFields,
+        metaclass=DataClassConstructor):
+    """A base class to define DataClasses."""
+
+    __data_base_class__ = True
+
     label = Field("""
-        A string to name a Data instance""",
+        A string to name a Data instance
+        """,
         __type__=str,
         __required__=False)
-    index = Field("""
-        A dict mapping the name of an index variable to its description.
-        The names will be used to name the dataframe index,
-        and the description to provide documentation.""",
-        __type__=dict)
-    measurements = Field("""
-        A dict mapping the name of measurement variable to its description.
-        The names will be used to name the dataframe columns, 
-        and the description to provide documentation.""",
-        __type__=dict)
-    contents = Field("""
-        A list of dicts that can be used to construct a pandas.DataFrame.""",
+    data = Field("""
+        A list of dicts that can be used to construct a pandas.DataFrame.
+        """,
         __type__=list,
-        __required__=False)
+        __required__=True)
+
+
+    def __init__(self,
+            data,
+            *args, **kwargs):
+        """..."""
+        for d in data:
+            for i in self.index.keys():
+                if i not in d.keys():
+                    raise ValueError(
+                        """Missing data index '{}' in dict {}""".format(i, d))
+            for m in self.measurements.keys():
+                if m not in d.keys():
+                    raise ValueError(
+                        """Missing measurement '{}' in dict {}""".format(m, d))
+
+        kwargs["data"] = data
+
+        super().__init__(
+            *args, **kwargs)
 
     @property
     def dataframe(self):
         """
         Data as a dataframe.
         """
-        return pd.DataFrame(self.contents)\
+        return pd.DataFrame(self.data)\
                  .set_index(list(self.index))
    
     @property
@@ -48,28 +116,3 @@ class Data(WithFields):
         """..."""
         return self.dataframe.iloc
 
-    def __call__(self, data_dicts, label):
-        """
-        Create Data instance containing actual data in addition to description.
-
-        Arguments
-        ------------
-        data_dicts :: list of dicts that provide single entry of the data.
-        Each of these dicts must have this Data instances' index names
-        and measurement names as keys.
-        """
-        for d in data_dicts:
-            for i in self.index.keys():
-                if i not in d.keys():
-                    raise ValueError(
-                        """Missing data index '{}' in dict {}""".format(i, d))
-            for m in self.measurements.keys():
-                if m not in d.keys():
-                    raise ValueError(
-                        """Missing measurement '{}' in dict {}""".format(i, m))
-
-        return Data(
-            label=label,
-            index=self.index,
-            measurements=self.measurements,
-            contents=data_dicts)
