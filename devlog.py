f8138f39000d1e65b585fcf74908cdbe5ed0c12f
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-06-13 10:57:29 +0200

    log in devlog.py
    This is an experiment in writing a developer's log,
    as python doc-string. It is a code-editor based notebook ---
    let us call it Python Journal.

---
+++ devlog.py
@@ -0,0 +1,18 @@
+"""
+Developer's log.
+Started 20190612,
+this is an attempt to log development notes as python code.
+"""
+
+from dmt.v2.tk.journal import *
+
+DevNote("""
+@<20190612-13:52>
+This is an example note.
+You may leave a time-stamp in the format shown above.
+Such a date will be picked up a parser of notes and used to index and
+track notes.
+Notice that we have not developed the code to parse dates from within
+the text of a note.
+""")
+
