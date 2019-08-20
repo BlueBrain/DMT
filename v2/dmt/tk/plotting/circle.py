37be095b371f2e36ab9ab04ad0cee4e227d43e31
Hugo Thabo Dictus <hugo.dictus@epfl.ch>
2019-08-20 14:49:52 +0200

    add docstrings

--- v2/dmt/tk/plotting/circle.py
+++ v2/dmt/tk/plotting/circle.py
@@ -4,10 +4,22 @@ import matplotlib.pyplot as plt
 from dmt.tk.enum import DATA_KEYS, MEAN
 from dmt.tk.plotting.utils import make_hashable
 
+
 # TODO: DOCSTRINGS DOCSTRINGS DOCSTRINGS
+# TODO: test that rotation works
 class CircleTool:
+    """a helper for drawing circles, circle segments, and arcs"""
 
     def __init__(self, radius, center=None, rotation=0):
+        """
+        initialize a circle with radius and center on the plot
+
+        Arguments:
+            radius: float, the radius of the circle
+            center: np.array (shp (2,)), center of the circle
+            rotation: float, rotation of the circle in radians
+                      0 (default) means that angle 0 points along y-axis
+        """
         if center is None:
             center = np.zeros((2,))
         self.center = center
@@ -15,20 +27,56 @@ class CircleTool:
         self.rot = rotation
 
     def angles_to_points(self, angles):
-        angles = np.array(angles)
-        xes = np.sin(angles + self.rot) * self.radius + self.center[0]
-        yes = np.cos(angles + self.rot) * self.radius + self.center[1]
+        """
+        get the coordinates on the circle at angle(s)
+
+        Arguments:
+            angles: number or array{number}. The angle(s) for which to get
+                    the points, in radians
+
+        Returns:
+            positions: (2,) or (N, 2) array of x and y coordinates at angle(s)
+        """
+
+        angles = np.array(angles) + self.rot
+        xes = np.sin(angles) * self.radius + self.center[0]
+        yes = np.cos(angles) * self.radius + self.center[1]
         if angles.shape == ():
             return np.array([xes, yes])
         return np.array([xes, yes]).transpose()
 
     def segment_points(self, from_angle, to_angle, N=50):
+        """
+        get N points along the circle between from_angle and to_angle
+
+        Arguments:
+           from_angle: number, start angle in radians
+           to_angle: number, end angle in radians
+           N: integer, number of points to get
+
+        Returns:
+            (N, 2) array of x and y positions on the circle
+        """
         return self.angles_to_points(np.linspace(from_angle, to_angle, N))
 
     def circle_points(self, N=100):
+        """
+        get N points around the full circle
+        """
         return self.segment_points(0, 2 * np.pi, N=N)
 
     def _arc_circle(self, from_angle, to_angle):
+        """
+        get a circle which intersects the two points on the current circle
+
+        Arguments:
+            from_angle: first intersection angle in radians
+            to_angle: second intersection angle in radians
+
+        Returns:
+            a CircleTool which intersects the first point at its own '0' angle
+            the angle in the returned circle where it intersects at to_angle
+        """
         mn_ngl = (from_angle + to_angle) / 2
         off_ngl = mn_ngl - np.minimum(from_angle, to_angle)
         ctr_ngl = np.pi - 2 * off_ngl
@@ -44,6 +92,18 @@ class CircleTool:
                           rotation=self.rot-rot_ngl-ctr_ngl), ctr_ngl
 
     def curve_points(self, from_angle, to_angle, N=100):
+        """
+        get N points representing a curve from from_angle on the circle
+        to to_angle on the circle
+
+        Arguments:
+            from_angle: the source angle in radians
+            to_angle: the target angle in radians
+            N: the number of points to represent the curve with
+
+        Returns:
+            (N, 2) array of points
+        """
         if np.isclose(np.abs(from_angle - to_angle), np.pi):
             start = self.angles_to_points(from_angle)
             end = self.angles_to_points(to_angle)
@@ -55,11 +115,28 @@ class CircleTool:
         return a.segment_points(0, ngl, N=N)
 
     def draw(self, N=100, *args, **kwargs):
+        """draw the full circle"""
         from matplotlib import pyplot as plt
         pts = self.circle_points(N=N)
         plt.plot(pts[:, 0], pts[:, 1], *args, **kwargs)
 
     def curve_polygon(self, from_angle1, from_angle2, to_angle1, to_angle2):
+        """
+        generate a matplotlib polygon of a curved path connecting two
+        segments on the circle.
+
+        (All angles in radians)
+
+        Arguments:
+            from_angle1: start angle of first curve, start of first segment
+            from_angle2: start angle of second curve, end of first segment
+            to_angle1: end angle of first curve, end of second segment
+            to_angle2: end angle of second curve, start of second segment
+
+        Returns:
+            a Polygon enclosed by the curves and segments
+        """
+
         xy = np.concatenate(
             [self.curve_points(from_angle1, to_angle1),
              self.segment_points(to_angle1, to_angle2),
@@ -68,6 +145,17 @@ class CircleTool:
         return matplotlib.patches.Polygon(xy, closed=False)
 
     def segment_polygon(self, from_angle, to_angle, width):
+        """
+        generate a matplotlib polygon representing a segment on the circle
+
+        Arguments:
+           from_angle: start angle of the segment, in radians
+           to_angle: end angle of the segment, in radians
+           width: the width of the polygon
+
+        returns:
+           a Polygon representing a ring segment
+        """
         outer = CircleTool(self.radius + width)
         xy = np.concatenate(
             [self.segment_points(from_angle, to_angle),
@@ -76,13 +164,34 @@ class CircleTool:
 
 
 class CirclePlot:
-
+    """
+    a plotter for circle-plots. CirclePlots consist of circle segments
+    connected by curves, the thickness of which corresponds to the weight
+    of the connection
+    """
     def __init__(self, space_between=0.0):
+        """
+        Arguments:
+           space_between : the space to leave between segments, in radians
+        """
         self.space_between = space_between
         self.circle = CircleTool(1.0)
 
     # TODO: test case where precol and postcol don't have same values
+    # TODO: test case where pre and post are just strings instead of
+    #       dicts
     def _prepare_plot(self, df):
+        """
+        convert the data into a pivot table for plotting
+        with mean as the values, and the two non-data columns
+        as the index and column values.
+
+        Arguments:
+            df: DataFrame with {MEAN} column
+
+        Returns:
+            pivot table of {MEAN}
+        """.format(MEAN=MEAN)
         non_data_columns = [col for col in df.columns
                             if col not in DATA_KEYS]
         assert len(non_data_columns) == 2
@@ -93,6 +202,16 @@ class CirclePlot:
         return pivot_table
 
     def type_angles(self, pivot_table):
+        """
+        find the start and end angles for each segment representing a
+        group in the table.
+
+        Arguments:
+            pivot_table: pandas pivot table with groups as index and columns
+
+        Returns:
+            dict {group: (start_angle, end_angle)}
+        """
         tot_conn = np.sum(pivot_table.values)
         type_angles = {}
         angle = 0
@@ -106,6 +225,24 @@ class CirclePlot:
         return type_angles
 
     def connection_angles(self, pivot_table, type_angles):
+        """
+        get the source and target angles for the curves representing
+        the connections between groups
+
+        Arguments:
+            pivot_table: pandas pivot table with groups as index and columns
+            type_angles: dict mapping group to the start and end angle
+
+        Returns:
+            tuple of dict (source_angles, dest_angles)
+            each of the form {<group1>: {<group1>: start angle, end angle,
+                                         <group2>: ...
+                                         ...}
+                              <group2>: {...}
+                               ...}
+            for all the groups
+            where start_angle and end_angle are the angles defining a segment
+        """
         source_angles = {ind: {} for ind in pivot_table.index}
         dest_angles = {ind: {} for ind in pivot_table.index}
         for i, ind in enumerate(pivot_table.index):
@@ -137,9 +274,20 @@ class CirclePlot:
 
         return source_angles, dest_angles
 
-    def connection_patches(self, pivot_table, type_patch_angles):
+    def connection_patches(self, pivot_table, type_angles):
+        """
+        get matplotlib polygons representing the connections between groups
+
+        Arguments:
+            pivot_table: pandas pivot_table
+            type_angles: dict mapping group to the start and end angle
+
+        Returns:
+            dict of the form {<group>: {<group>: a::Polygon, ...} ...}
+            for all the groups in pivot_table
+        """
         source_angles, dest_angles =\
-            self.connection_angles(pivot_table, type_patch_angles)
+            self.connection_angles(pivot_table, type_angles)
         patches = {from_:
                    {to: self.circle.curve_polygon(*source_angles[from_][to],
                                                   *dest_angles[from_][to])
@@ -148,11 +296,31 @@ class CirclePlot:
         return patches
 
     def type_patches(self, type_angles):
+        """
+        get matplotlib polygons representing the locations of groups
+        on the circle
+
+        Arguments:
+            type_angles: dict mapping group to the start and end angle
+        """
         return {
             typ: self.circle.segment_polygon(*angles, 0.1)
             for typ, angles in type_angles.items()}
 
     def get_patches(self, pivot_table):
+        """
+        get the patches for both connections and groups
+
+        Arguments:
+            pivot_table: pandas pivot_table
+
+        Returns:
+            type_patches: dict mapping groups to representative polygons
+                          see type_patches method
+            connection_patches: dict mapping groups to dicts mapping groups to
+                                polygons representing the connection
+                                see connection_patches method
+        """
         type_angles = self.type_angles(pivot_table)
         connection_patches = self.connection_patches(pivot_table,
                                                      type_angles)
@@ -161,6 +329,20 @@ class CirclePlot:
 
     # TODO: could do with some tests
     def patch_collections(self, type_patches, connection_patches):
+        """
+        turn the patch_dicts from get_patches into colored PatchCollections
+
+        Arguments:
+            type_patches: dict mapping groups to representative polygons
+                          see type_patches method
+            connection_patches: dict mapping groups to dicts mapping groups to
+                                polygons representing the connection
+                                see connection_patches method
+
+        Returns:
+            PatchCollection of type_patches
+            PatchCollection of connection_patches
+        """
         types = sorted(type_patches.keys())
         type_color = np.arange(len(types))
         type_patch_list = [type_patches[typ] for typ in types]
@@ -186,6 +368,20 @@ class CirclePlot:
     #        its supposed to do
     # TODO: test how it handles NaNs
     def plot(self, df):
+        """"
+        create a CirclePlot of the data in df
+
+        Arguments:
+             df: a pandas DataFrame which must have a {MEAN} column,
+                 may have any of {DATA_KEYS} columns,
+                 and must have exactly two other columns
+                 the entries of which will define the groups to plot
+                 the {MEAN} column will determine the connection weight
+                 between the corresponding groups
+
+        Returns:
+            figure, axis with CirclePlot
+        """.format(MEAN=MEAN, DATA_KEYS=DATA_KEYS)
         pivot_table = self._prepare_plot(df)
         assert sorted(pivot_table.index) == sorted(pivot_table.columns)
         ax = plt.axes()
@@ -208,7 +404,7 @@ class CirclePlot:
         textcirc = CircleTool(self.circle.radius * 1.2)
         for t, a in type_angles.items():
             plt.text(*textcirc.angles_to_points(type_angles[t]), t)
-        return ax
+        return plt.gcf(), ax
 
 
 # CirclePlot
