897f9697d7b6196c0b19109968800685827306a3
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-08-28 15:20:23 +0200

    move cell density analysis to library/user/visood

--- v2/dmt/tk/collections/__init__.py
+++ v2/dmt/tk/collections/__init__.py
@@ -2,4 +2,44 @@
 Custom collection classes and methods.
 """
 
+
+"""
+Utilities to deal with collections.
+"""
+
+import collections
+from itertools import *
+
+
+def take(n, xs):
+    """
+    Take n elements from a collection
+
+    Parameters
+    ---------------------------------------------------------------------------
+    n :: int #number of items to retrieve
+    xs :: iterable #...
+    """
+    return islice(xs, n)
+
+def drop(n, xs):
+    """
+    Drop n elements from an iterable.
+    """
+    return islice(xs, n, None)
+
+def step(n, xs):
+    """
+    Step through an iterable.
+    """
+    return islice(xs, None, None, n)
+
+def check(xs):
+    """
+    Check xs is a collection.
+    """
+    return(
+        isinstance(xs, collections.Iterable) and
+        not isinstance(xs, (str, bytes)) )
+
 from .emuset import emuset
