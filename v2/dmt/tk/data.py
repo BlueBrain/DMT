04328cf86ebbb55467c84f6a6652596537279f73
Hugo Thabo Dictus <hugo.dictus@epfl.ch>
2019-09-02 14:55:39 +0200

    implement recursion for nested values.
    function is butt-ugly though

--- v2/dmt/tk/data.py
+++ v2/dmt/tk/data.py
@@ -2,15 +2,17 @@ import pandas as pd
 from collections import Mapping, Iterable
 
 
-def parameters_to_df(parameters):
+def parameters_to_df(parameters, rdepth=0):
     """
     given a set of parameter values return a dataframe with
     multiindexed columns for dict-like parameter values, converting
     any lists to tuples
     """
     base_df = pd.DataFrame(parameters)
+
     lv1 = list(base_df.columns)
     lv2 = [[] for c in lv1]
+    at_least_one_sub = False
     for c, col in enumerate(lv1):
         has_noniter = False
         has_iter = False
@@ -20,24 +22,33 @@ def parameters_to_df(parameters):
                     if k not in lv2[c]:
                         lv2[c].append(k)
                 has_iter = True
-            elif isinstance(v, Iterable) and not isinstance(v, str):
+                at_least_one_sub = True
+            elif (
+                isinstance(v, Iterable) and
+                    (not isinstance(v, str) and not isinstance(v, set))):
                 for n, _ in enumerate(v):
                     if n not in lv2[c]:
                         lv2[c].append(n)
                 has_iter = True
+                at_least_one_sub = True
             else:
                 has_noniter = True
 
         if (has_noniter and has_iter) and 0 not in lv2[c]:
             lv2[c].append(0)
 
+    if not at_least_one_sub:
+        return base_df
 
     newdfdict = {}
     for i, l1 in enumerate(lv1):
         if len(lv2[i]) == 0:
             newdfdict[l1] = base_df[l1].values
         for j, l2 in enumerate(lv2[i]):
-            key = (l1, l2)
+            if isinstance(l1, tuple):
+                key = l1 + (l2, )
+            else:
+                key = (l1, l2)
             values = []
             for value in base_df[l1]:
                 try:
@@ -51,4 +62,4 @@ def parameters_to_df(parameters):
 
             newdfdict[key] = values
 
-    return pd.DataFrame(newdfdict)
+    return pd.DataFrame(parameters_to_df(newdfdict, rdepth+1))
