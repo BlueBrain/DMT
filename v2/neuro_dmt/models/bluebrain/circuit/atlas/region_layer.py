3d9db201deff61849d54c4dc09f53870020847ca
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-09-12 20:45:39 +0200

    Use abstract/concrete pattern in representation
    Bridge pattern does not really fit,
    and causes unnecessary code duplication.

--- v2/neuro_dmt/models/bluebrain/circuit/atlas/region_layer.py
+++ v2/neuro_dmt/models/bluebrain/circuit/atlas/region_layer.py
@@ -8,11 +8,27 @@ from dmt.tk import collections
 from neuro_dmt.terminology.atlas import translate
 
 
-class RegionLayerRepresentationImplementation(ABCWithFields):
+class RegionLayerRepresentation(
+        ABCWithFields):
     """
-    Implements `RegionLayerRepresentation`.
-    """
+    Express how the circuit atlas represents laminar regions of a brain area
+    such as the cortex.
 
+    A brain atlas organizes brain regions in a hierarchy. Regions such as
+    the primary somatosensory, and the primary somatosensory area lower limb
+    fit naturally in this hierarchy based on 'is-contained-in' relationships.
+    Layers, however, form a structure that is orthogonal to the regions in this
+    hierarchy. Layers span all the regions in the cortex. To fit them in the
+    atlas's hierarchy, each final cortical region is divided into cortical
+    layers, each of which form a leaf region. Thus each cortical layer is
+    chunked up and annotated by the (sub-)region containing it. The volumetric 
+    data associated with the atlas uses only the ids associated with the leaf
+    regions of the atlas hierarchy. Sticking chunked up layer regions into the
+    hierarchy as leaf nodes allows the brain circuit atlas to use only a single
+    volumetric data set. While there are only a few brain region naming
+    conventions, there are no standards about how to name the layer regions. We
+    track the complexity arising out of this missing naming convention.
+    """
     applicable_patterns = ClassAttribute(
         """
         Regex patterns that can be used to resolve how the atlas represents
@@ -34,9 +50,23 @@ class RegionLayerRepresentationImplementation(ABCWithFields):
             for pattern in ("SSCtx", "S1HL"))
 
     @classmethod
+    def for_atlas(cls, atlas):
+        """
+        An instance for the given `Atlas` instance.
+        """
+        for concrete_class in (
+                FullLayerRepresentation,
+                SemicolonIntRepresentation,
+                BlueBrainAtlasRepresentation):
+            if concrete_class.is_applicable(atlas):
+                return concrete_class(atlas)
+        raise Exception(
+            "No available implementation applies to atlas {}.".format(atlas))
+
+    @classmethod
     def is_applicable(cls, atlas):
         """
-        Is this implementation applicable to given `Atlas` instance.
+        Is this concrete implementation applicable to given `Atlas` instance.
         """
         return any(
             atlas.load_region_map().find(pattern, attr="acronym")
@@ -51,7 +81,7 @@ class RegionLayerRepresentationImplementation(ABCWithFields):
                region
 
     @abstractclassmethod
-    def get_query_layer(self, layer):
+    def get_query_layer(cls, layer):
         """
         Get layer as the atlas represents it.
         """
@@ -68,7 +98,7 @@ class RegionLayerRepresentationImplementation(ABCWithFields):
                        
 
 class FullLayerRepresentation(
-        RegionLayerRepresentationImplementation):
+        RegionLayerRepresentation):
     """
     Layer region acronyms contain the full layer string, e.g. L2 or mc2;L2.
     """
@@ -83,7 +113,7 @@ class FullLayerRepresentation(
 
 
 class SemicolonIntRepresentation(
-        RegionLayerRepresentationImplementation):
+        RegionLayerRepresentation):
     """
     Layer region acronyms separated region from layer by a semicolon,
     <region>;<layer>
@@ -99,7 +129,7 @@ class SemicolonIntRepresentation(
 
 
 class BlueBrainAtlasRepresentation(
-        RegionLayerRepresentationImplementation):
+        RegionLayerRepresentation):
     """
     Convention used, or to be used by the BlueBrain Atlas.
     """
@@ -116,67 +146,3 @@ class BlueBrainAtlasRepresentation(
         return layer[1:]\
             if layer.startswith('L') and layer[1] in "123456"\
                else layer.lower()
-
-
-
-class RegionLayerRepresentation:
-    """
-    Express how the circuit atlas represents laminar regions of a brain area
-    such as the cortex.
-
-    A brain atlas organizes brain regions in a hierarchy. Regions such as
-    the primary somatosensory, and the primary somatosensory area lower limb
-    fit naturally in this hierarchy based on 'is-contained-in' relationships.
-    Layers, however, form a structure that is orthogonal to the regions in this
-    hierarchy. Layers span all the regions in the cortex. To fit them in the
-    atlas's hierarchy, each final cortical region is divided into cortical
-    layers, each of which form a leaf region. Thus each cortical layer is
-    chunked up and annotated by the (sub-)region containing it. The volumetric 
-    data associated with the atlas uses only the ids associated with the leaf
-    regions of the atlas hierarchy. Sticking chunked up layer regions into the
-    hierarchy as leaf nodes allows the brain circuit atlas to use only a single
-    volumetric data set. While there are only a few brain region naming
-    conventions, there are no standards about how to name the layer regions. We
-    track the complexity arising out of this missing naming convention.
-
-    We will use a version of the bridge pattern:
-    1. https://www.giacomodebidda.com/bridge-pattern-in-python/
-    2. https://sourcemaking.com/design_patterns/bridge
-    3. https://simpleprogrammer.com/design-patterns-simplified-the-bridge-pattern/
-    """
-
-    def __init__(self, implementation):
-        """
-        Initialize with the given concrete `implementation`.
-
-        A concrete implementation will be used to provide functionality to this
-        `interface`.
-        """
-        self._implementation = implementation
-
-    @classmethod
-    def for_atlas(cls, atlas):
-        """
-        An instance for the given `Atlas` instance.
-        """
-        for implementation_class in (
-                FullLayerRepresentation,
-                SemicolonIntRepresentation,
-                BlueBrainAtlasRepresentation):
-            if implementation_class.is_applicable(atlas):
-                return cls(implementation_class(atlas))
-        raise Exception(
-            "No available implementation applies to atlas {}.".format(atlas))
-
-    def get_region_acronym(self, region):
-        """
-        Acronym for a region.
-        """
-        return self._implementation.get_region_acronym(region)
-
-    def get_layer_region_regex(self, region):
-        """
-        Get acronym for region.
-        """
-        return self._implementation.get_layer_region_regex(region)
-
