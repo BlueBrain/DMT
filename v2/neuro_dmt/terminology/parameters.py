5b05af547cfbfbf4d4f6b04c65ad41cf60cd7fd3
Hugo Thabo Dictus <hugo.dictus@epfl.ch>
2019-09-19 13:51:59 +0200

    add parameter terminology to neuro_dmt

---
+++ v2/neuro_dmt/terminology/parameters.py
@@ -0,0 +1,16 @@
+# TODO: we can put together parameter classes
+#       they should __repr__ and __hash__ as their string form, but may
+#       contain additional data and functionality
+#       HOWEVER, since we don't know what that metadata and functionality is
+#       for now, strings give us exactly the functionality we need
+LAYER = 'layer'
+MTYPE = 'mtype'
+SYNAPSE_CLASS = "synapse_class"
+MORPH_CLASS = "morphology_class"
+MESOCOLUMN = "mesocolumn"
+HYPERCOLUMN = "hypercolumn"
+BRAIN_REGION = "brain_region"
+PRESYNAPTIC = 'pre'
+POSTSYNAPTIC = 'post'
+DEPTH = 'depth'
+HEIGHT = 'height'
