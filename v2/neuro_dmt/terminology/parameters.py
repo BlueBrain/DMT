581c2a73bba7587ada2c7b26269cc0c77997a635
Hugo Thabo Dictus <hugo.dictus@epfl.ch>
2019-09-20 11:38:21 +0200

    Make parameters in neuro_dmt actually Parameter instances
    also rename BRAIN_REGION back to 'region'

--- v2/neuro_dmt/terminology/parameters.py
+++ v2/neuro_dmt/terminology/parameters.py
@@ -1,16 +1,13 @@
-# TODO: we can put together parameter classes
-#       they should __repr__ and __hash__ as their string form, but may
-#       contain additional data and functionality
-#       HOWEVER, since we don't know what that metadata and functionality is
-#       for now, strings give us exactly the functionality we need
-LAYER = 'layer'
-MTYPE = 'mtype'
-SYNAPSE_CLASS = "synapse_class"
-MORPH_CLASS = "morphology_class"
-MESOCOLUMN = "mesocolumn"
-HYPERCOLUMN = "hypercolumn"
-BRAIN_REGION = "brain_region"
-PRESYNAPTIC = 'pre'
-POSTSYNAPTIC = 'post'
-DEPTH = 'depth'
-HEIGHT = 'height'
+from dmt.tk.terminology.parameters import Parameter
+
+LAYER = Parameter('layer', 'a layer of some brain region')
+MTYPE = Parameter('mtype', 'mtype classification')
+SYNAPSE_CLASS = Parameter("synapse_class", 'synapse class of cell, excitatory, inhibitory, etc.')
+MORPH_CLASS = Parameter("morphology_class", "class of morphology, such as pyramidal, interneuron")
+MESOCOLUMN = Parameter("mesocolumn", "not actually sure tbh")
+HYPERCOLUMN = Parameter("hypercolumn", "yeah... this isn't really clear")
+BRAIN_REGION = Parameter("region", "region of the brain, as BBA-style acronym")
+PRESYNAPTIC = Parameter('pre', "presynaptic cell group, as a dict of other parameters")
+POSTSYNAPTIC = Parameter('post', "postsynaptic cell group as a dict of other parameters")
+DEPTH = Parameter('depth', "depth into region")
+HEIGHT = Parameter('height', "height from bottom of region")
