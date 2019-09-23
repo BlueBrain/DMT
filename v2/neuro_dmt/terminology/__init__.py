4b3c0ae02b81524f8311bb1988470fe379ffc533
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-09-23 10:50:24 +0200

    Define neuro_dmt terminology
    And add a simple test.

--- v2/neuro_dmt/terminology/__init__.py
+++ v2/neuro_dmt/terminology/__init__.py
@@ -1,3 +1,70 @@
 """
 Neuroscience terminology used.
 """
+from dmt.tk.terminology import *
+from dmt.tk.utils.singleton import Singleton
+
+
+class circuit(Singleton):
+    """
+    A collection of terms used in neuroscience.
+    """
+    region = Term(
+        "region",
+        "A region / area in the brain.")
+    layer = Term(
+        "layer",
+        "A structure in certain brain areas, such as the cortex")
+    depth = Term(
+        "depth",
+        "Depth into a brain region, measured absolutely.")
+    height = Term(
+        "height",
+        "Height into a brain region, measured absolutely.")
+    mesocolumn  = Term(
+        "mesocolumn",
+        """
+        Organizational structure of model brain circuits that corresponds
+        to cortical columns. A meso-column is a collection of micro-column.
+        """)
+    hypercolumn = Term(
+        "hypercolumn",
+        """
+        Used at BBP, probably the same as a meso-column.
+        """)
+
+
+class cell(Singleton):
+    """
+    A collection of terms used in neuroscience.
+    """
+    mtype = Term(
+        "mtype",
+        "Label assigned to cells to mark their morphology.")
+    etype = Term(
+        "etype",
+        "Label assigned to cells to mark their electro-physiology.")
+    synapse_class = Term(
+        "synapse_class",
+        "Synapse class of a cell can be either excitatory, or inhibitory")
+    postsynaptic = Term(
+        "postsynaptic",
+        "Indiciates that the cell is on the post side of a synapse.")
+    presynaptic = Term(
+        "presynaptic",
+        "Indicates that the cell is on the pre side of a synapse.")
+
+
+class synapse(Singleton):
+    """
+    A collection of terms used in neuroscience.
+    """
+    afferent = Term(
+        "afferent",
+        "An afferent synapse, or connection, enters a cell.")
+    efferent = Term(
+        "efferent",
+        "An efferent synapse, or connection exits a cell.")
+
+
+
