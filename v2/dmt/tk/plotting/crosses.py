6b3f65e126b81455a9416e79ac79e9188fdf80f2
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-10-08 15:43:16 +0200

    implement cross comparison plot

---
+++ v2/dmt/tk/plotting/crosses.py
@@ -0,0 +1,206 @@
+"""
+Plot crosses to compare two datasets.
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+import pandas as pd
+import seaborn
+from dmt.data.observation import measurement
+from dmt.tk.field import Field, lazyfield, WithFields
+from .import golden_aspect_ratio
+from .figure import Figure
+
+class Crosses(WithFields):
+    """
+    Specifies a plotting method, and all its required attributes.
+    """
+    title = Field(
+        """
+        Title to be displayed.
+        If not provided phenomenon for the data will be used.
+        """,
+        __default_value__="")
+    xvar = Field(
+        """
+        Column in the data-frame to be plotted along the x-axis.
+        """)
+    yvar = Field(
+        """
+        Column in the data-frame to be plotted along the y-axis.
+        """)
+    gvar = Field(
+        """
+        Column in the data-frame that will be plotted as different colored
+        bars (at the same value of `yvar`).
+        A default of empty string will be interpreted as not set, and hence
+        no column in the data-frame to use as a grouping variable.
+        """)
+    xlabel = Field(
+        """
+        The label to be displayed along the y-axis.
+        Default value of an empty string will cause `xvar` to be displayed
+        as `xlabel`.
+        """,
+        __default_value__="")
+    ylabel = Field(
+        """
+        The label to be displayed along the y-axis.
+        Default value of an empty string will cause `yvar` to be displayed
+        as `ylabel`.
+        """,
+        __default_value__="")
+    fmt = Field(
+        """
+        Point type to be plotted.
+        """,
+        __default_value__='o')
+    size_figure = Field(
+        """
+        Height of the figure.
+        """,
+        __default_value__ = 8.)
+    aspect_ratio_figure = Field(
+        """
+        Aspect ratio width / height for the figure.
+        """,
+        __default_value__=golden_aspect_ratio)
+    confidence_interval = Field(
+        """
+        float or “sd” or None, optional
+        Size of confidence intervals to draw around estimated values.
+        If “sd”, skip bootstrapping and draw the standard deviation of the
+        observations. If None, no bootstrapping will be performed,
+        and error bars will not be drawn.
+        """,
+        __default_value__="sd")
+
+    @lazyfield
+    def xerr(self):
+        """
+        Column that will contain errors associated with `xvar`.
+        """
+        return "{}_err".format(self.xvar)
+
+    @lazyfield
+    def yerr(self):
+        """
+        Column that will contain errors associated with `yvar`.
+        """
+        return "{}_err".format(self.yvar)
+
+    @staticmethod
+    def _get_phenomenon(dataframe_long):
+        """
+        Get phenomenon to be plotted.
+        """
+        return dataframe_long.columns[0][0]
+
+    def _get_plotting_data(self, dataframe_long):
+        """
+        Convert `data` to plotting data.
+        """
+        datasets =\
+            dataframe_long.reset_index().dataset.unique()
+        if len(datasets) != 2:
+            raise TypeError(
+                """
+                Dataframe for plotting has more than two datasets: {}
+                """.format(datasets))
+        phenomenon =\
+            self._get_phenomenon(dataframe_long)
+        dataframe_x =\
+            dataframe_long.xs(self.xvar, level="dataset")[phenomenon]\
+                     .dropna()
+        dataframe_y =\
+            dataframe_long.xs(self.yvar, level="dataset")[phenomenon]\
+                     .dropna()
+        if dataframe_x.shape[0] != dataframe_y.shape[0]:
+            raise TypeError(
+                """
+                Dataframe for plotting had different number of elements
+                for the two datasets to be plotted:
+                {}: {}
+                {}: {}
+                """.format(
+                    self.xvar, dataframe_x.shape[0],
+                    self.yvar, dataframe_y.shape[0]))
+        return\
+            pd.DataFrame(
+                {self.xvar: dataframe_x["mean"].values,
+                 self.yvar: dataframe_y["mean"].values,
+                 self.xerr: dataframe_x["std"].values,
+                 self.yerr: dataframe_y["std"].values},
+                index=dataframe_x.index)\
+              .reset_index()
+
+    def _get_title(self, dataframe_long):
+        """
+        Get a title to display.
+        """
+        if self.title:
+            return self.title
+        phenomenon = self._get_phenomenon(dataframe_long)
+        return ' '.join(word.capitalize() for word in phenomenon.split('_'))
+
+    def get_figure(self,
+            data,
+            *args,
+            caption="Caption not provided",
+            **kwargs):
+        """
+        Plot the dataframe.
+        """
+        dataframe_long = measurement.concat_as_summaries(data)
+        dataframe_wide = self._get_plotting_data(dataframe_long)
+        graphic =\
+            seaborn.FacetGrid(
+                dataframe_wide,
+                hue=self.gvar,
+                size=self.size_figure,
+                legend_out=True)
+        graphic.map(
+            plt.errorbar,
+            self.xvar,
+            self.yvar,
+            self.xerr,
+            self.yerr,
+            fmt=self.fmt)
+        graphic.add_legend()
+        graphic.fig.suptitle(self._get_title(dataframe_long))
+        limits =[
+            np.maximum(
+                np.nanmax(
+                    dataframe_wide[self.xvar] + dataframe_wide[self.xerr]),
+                np.nanmax(
+                    dataframe_wide[self.yvar] + dataframe_wide[self.yerr])),
+            np.minimum(
+                np.nanmin(
+                    dataframe_wide[self.xvar] - dataframe_wide[self.xerr]),
+                np.nanmin(
+                    dataframe_wide[self.yvar] - dataframe_wide[self.yerr]))]
+        plt.plot(limits, limits, "k--")
+        return Figure(
+            graphic.set(
+                xlabel=self.xlabel if self.xlabel else self.xvar,
+                ylabel=self.ylabel if self.ylabel else self.yvar),
+            caption=caption)
+
+    def plot(self,
+            dataframe,
+            *args, **kwargs):
+        """
+        Plot the dataframe
+        """
+        return self\
+            .get_figure(
+                dataframe,
+                *args, **kwargs)
+
+    def __call__(self,
+            dataframe):
+        """
+        Make this class a callable,
+        so that it can masquerade as a function!
+        """
+        return self.plot(dataframe)
