edd2ddfb0a2df5279b3604e0b79b5fb58c7ed2b6
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-11-07 16:56:54 +0100

    test develop CellType

---
+++ v2/neuro_dmt/models/bluebrain/circuit/model/test/cell_type.py
@@ -0,0 +1,100 @@
+"""
+Test develop CellType.
+"""
+import pandas as pd
+from neuro_dmt.models.bluebrain.circuit.model.cell_type import CellType
+
+def test_cell_types():
+    """
+    `CellType` should initialize from a `dict` or a `pandas.Series`,
+    and should evaluate specifiers.
+    """
+    expected_specifiers = frozenset(("mtype", "layer", "etype"))
+
+
+    cell_types = (
+        CellType(dict(
+            (("mtype", "L23_MC"), ("layer", "L3"),     ("etype", "bNAC")))),
+        CellType(dict(
+            (("mtype", "L23_MC"), ("etype", "bNAC"),   ("layer", "L3")))),
+        CellType(dict(
+            (("layer", "L3"),     ("mtype", "L23_MC"), ("etype", "bNAC")))),
+        CellType(dict(
+            (("layer", "L3"),     ("etype", "bNAC"),   ("mtype", "L23_MC")))),
+        CellType(dict(
+            (("etype", "bNAC"),   ("mtype", "L23_MC"), ("layer", "L3")))),
+        CellType(dict(
+            (("etype", "bNAC"),   ("layer", "L2"),     ("mtype", "L23_MC")))))
+
+    for cell_type in cell_types:
+        assert isinstance(cell_type.value, pd.Series)
+        assert len(cell_type.value) == 3
+    
+        assert cell_types[0].specifier == expected_specifiers,\
+            cell_types[1].specifier
+        assert cell_types[1].specifier == expected_specifiers,\
+            cell_types[2].specifier
+        assert cell_types[2].specifier == expected_specifiers,\
+            cell_types[3].specifier
+        assert cell_types[3].specifier == expected_specifiers,\
+            cell_types[4].specifier
+        assert cell_types[4].specifier == expected_specifiers,\
+            cell_types[5].specifier
+        assert cell_types[5].specifier == expected_specifiers,\
+            cell_types[6].specifier
+
+    cell_types = (
+        CellType(pd.Series(
+            ["L23_MC", "L3", "bNAC"], index=["mtype", "layer", "etype"])),
+        CellType(pd.Series(
+            ["L23_MC", "bNAC", "L3"], index=["mtype", "etype", "layer"])),
+        CellType(pd.Series(
+            ["L3", "L23_MC", "bNAC"], index=["layer", "mtype", "etype"])),
+        CellType(pd.Series(
+            ["L3", "bNAC", "L23_MC"], index=["layer", "etype", "mtype"])),
+        CellType(pd.Series(
+            ["bNAC", "L23_MC", "L3"], index=["etype", "mtype", "layer"])),
+        CellType(pd.Series(
+            ["bNAC", "L3", "L23_MC"], index=["etype", "layer", "mtype"])))
+
+    for cell_type in cell_types:
+        assert isinstance(cell_type.value, pd.Series)
+        assert len(cell_type.value) == 3
+    
+        assert cell_types[0].specifier == expected_specifiers,\
+            cell_types[1].specifier
+        assert cell_types[1].specifier == expected_specifiers,\
+            cell_types[2].specifier
+        assert cell_types[2].specifier == expected_specifiers,\
+            cell_types[3].specifier
+        assert cell_types[3].specifier == expected_specifiers,\
+            cell_types[4].specifier
+        assert cell_types[4].specifier == expected_specifiers,\
+            cell_types[5].specifier
+        assert cell_types[5].specifier == expected_specifiers,\
+            cell_types[6].specifier
+        
+def test_cell_type_pathway():
+    """
+    `CellType` should be able to compute a pathway,
+    with or without a `pandas.MultiIndex`.
+    """
+    pathway =\
+        CellType.pathway(
+            {"mtype": "L23_MC"},
+            {"mtype": "L23_MC"})
+    assert pathway.pre_synaptic.mtype == "L23_MC", pathway
+    assert pathway.post_synaptic.mtype == "L23_MC", pathway
+
+    pathway =\
+        CellType.pathway(
+            {"mtype": "L23_MC"},
+            {"mtype": "L23_MC"},
+            multi_indexed=False)
+    assert pathway.pre_synaptic_mtype == "L23_MC", pathway
+    assert pathway.post_synaptic_mtype == "L23_MC", pathway
+
+    
+
+    
+
