b1e1fbb900ade965e0b26ef5732b8c81f7497f75
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-12-02 17:08:27 +0100

    add cell ratio data to neuro_dmt.data.rat

--- v2/neuro_dmt/data/rat.py
+++ v2/neuro_dmt/data/rat.py
@@ -4,6 +4,7 @@ Datasets used / created at the Blue Brain Project for the rat.
 
 import os
 from collections import OrderedDict
+import numpy
 import pandas
 from dmt.tk.phenomenon import Phenomenon
 from dmt.tk.utils import datasets
@@ -95,3 +96,87 @@ meyer2010 =\
         ).assign(
             layer=layers))
 
+
+class ByLayerInhibitoryCellFractionMeasurement(SampleMeasurement):
+    """
+    Measurement of inhibitory cell fraction, by cortical layer.
+    """
+    phenomenon = Phenomenon(
+        "Inhibitory Cell Fraction",
+        description = "Fraction of inhibitory cells.",
+        group="Composition"
+    )
+    parameters = OrderedDict(
+        layer="Cortical layer where the inhibitory cell fraction was measured"
+    )
+
+
+__path_inhibitory_cell_fraction_datasets =\
+    os.path.join(
+        data.LOCATION,
+        "cortex/sscx/rat/composition",
+        "cell_ratio")
+
+__ghobril_2012_dataset =\
+    datasets.load(
+        __path_inhibitory_cell_fraction_datasets,
+        "Ghobril2012")
+__ghobril_count_inhibitory =\
+    numpy.array(__ghobril_2012_dataset["count_inh"])
+__ghobril_count_total =\
+    numpy.array(__ghobril_2012_dataset["count_all"])
+__ghobril_mean =\
+    __ghobril_count_inhibitory / __ghobril_count_total
+__ghobril_std =\
+    __ghobril_mean * numpy.sqrt(
+        1./__ghobril_count_inhibitory + 1./__ghobril_count_total)
+ghobril2012 =\
+    ByLayerInhibitoryCellFractionMeasurement.SummaryType()(
+        label=__ghobril_2012_dataset["short_name"],
+        object_of_observation="Multi-stain confocal images",
+        procedure=__ghobril_2012_dataset["what"],
+        provenance=__ghobril_2012_dataset["citation"],
+        citation=__ghobril_2012_dataset["citation"],
+        uri=__ghobril_2012_dataset["uri"],
+        data=pandas.DataFrame({
+            ("inhibitory_cell_fraction", "mean"): __ghobril_mean,
+            ("inhibitory_cell_fraction", "std"): __ghobril_std,
+        }).assign(
+            layer=layers))
+
+
+__lefort2009_dataset =\
+    datasets.load(
+        __path_inhibitory_cell_fraction_datasets,
+        "Lefort2009")
+lefort2009 =\
+    ByLayerInhibitoryCellFractionMeasurement.SummaryType()(
+        label=__lefort2009_dataset["short_name"],
+        object_of_observation=__lefort2009_dataset["animal"],
+        procedure="Not Available, read the paper",
+        provenance="Read the paper",
+        citation=__lefort2009_dataset["citation"],
+        uri=__lefort2009_dataset["uri"],
+        data=pandas.DataFrame({
+            ("inhibitory_cell_fraction", "mean"): __lefort2009_dataset["ratio_means"],
+            ("inhibitory_cell_fraction", "std"): __lefort2009_dataset["ratio_stds"]
+        }).assign(
+            layer=layers))
+
+__beaulieu1992_dataset =\
+    datasets.load(
+        __path_inhibitory_cell_fraction_datasets,
+        "Beaulieu1992")
+beaulieu1992 =\
+    ByLayerInhibitoryCellFractionMeasurement.SummaryType()(
+        label=__beaulieu1992_dataset["short_name"],
+        object_of_observation=__beaulieu1992_dataset["animal"],
+        procedure="Not Available, read the paper",
+        provenace="Read the paper",
+        citation=__beaulieu1992_dataset["citation"],
+        uri=__beaulieu1992_dataset["uri"],
+        data=pandas.DataFrame({
+            ("inhibitory_cell_fraction", "mean"): __beaulieu1992_dataset["ratio_means"],
+            ("inhibitory_cell_fraction", "std"): __beaulieu1992_dataset["ratio_stds"]
+        }).assign(
+            layer=layers))
