06e9482399e2ecd76a76c7b6b7658e1d67a3153e
Vishal Sood <vishal.chandra.sood@gmail.com>
2020-03-04 12:55:39 +0100

    pretty print record

--- v2/dmt/tk/field/record.py
+++ v2/dmt/tk/field/record.py
@@ -6,12 +6,25 @@ import random
 from .import Field, WithFields
 
 def Record(**kwargs):
-    fields = {
+    attributes = {
         key: Field("record field {}".format(key))
         for key in kwargs.keys()}
+
+    def __repr__(self):
+        return\
+            '\n'.join(
+                "{}: {}".format(field, repr(value).replace('\n', "; "))
+                for field, value in self.field_dict.items())
+
+    attributes["__str__"] = __repr__
+    attributes["__repr__"] = __repr__
+
     record_type =\
         type(
             "Record{}".format(random.randint(0, 1000000000)),
             (WithFields,),
-            fields)
+            attributes)
     return record_type(**kwargs)
+
+    
+
