ab8c28f1e981024eefec2dfea22eb05c079ca177
Vishal Sood <vishal.chandra.sood@gmail.com>
2020-03-27 23:14:06 +0100

    start composition analyses suite

--- neuro_dmt/library/analyses/circuit/composition/suite.py
+++ neuro_dmt/library/analyses/circuit/composition/suite.py
@@ -13,44 +13,23 @@
 # You should have received a copy of the GNU General Public License along with
 # DMT source-code.  If not, see <https://www.gnu.org/licenses/>. 
 
-"""
-Analyses and validations for brain circuits
-"""
+from ..import *
 
-import sys
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from dmt.data.observation import measurement
-from dmt.tk.journal import Logger
-from dmt.tk.phenomenon import Phenomenon
-from dmt.tk.parameters import Parameters
-from dmt.tk.plotting import Bars, LinePlot, HeatMap, MultiPlot
-from dmt.tk.field import NA, Record, Field, LambdaField, lazyfield, WithFields
-from neuro_dmt import terminology
-from neuro_dmt.analysis.circuit import BrainCircuitAnalysis
-from neuro_dmt.analysis.reporting import\
-    CircuitProvenance, CircuitAnalysisReport, CheetahReporter
-from ...models.bluebrain.circuit.atlas import BlueBrainCircuitAtlas
-from ...models.bluebrain.circuit.model import BlueBrainCircuitModel
-from ...models.bluebrain.circuit.adapter import BlueBrainCircuitAdapter
+LOGGER = Logger(client=__file__)
 
-
-
-class AnalysisSpec(WithFields):
+class CompositionAnalysisSuite(WithFields):
     """
-    Specify a brain-circuit analysis.
+    Analyze the composition of a brain circuit model.
     """
     sample_size = Field(
         """
-        Number of individual sample measurements for each set of
-        parameter values.
+        Number of individual sample measurements for each set of parameter
+        values.
         """,
         __default_value__=100)
     size_roi = Field(
         """
-        Size of ROIs that will be sampled in circuit's physical space.
+        Size of ROIs that composition phenomena will be computed in.
         """,
         __default_value__=50. * np.ones(3))
     path_reports = Field(
@@ -60,12 +39,12 @@ class AnalysisSpec(WithFields):
         __default_value__=os.path.join(os.getcwd(), "reports"))
     morphologies_interneurons = Field(
         """
-        Interneuron morphologies that are stained by markers.
+        Interneuron morphologies that are stained by markers",
         """,
         __default_value__=[
-            "BP", "BTC", "CHC", "DB", "LBC", "NBC", "MC", "SBC", "SSC"])
+            "BP", "BTC", "CHC", "DB", "LBC", "NBC", "MC", "SBC", "SSC"])],
     number_cortical_thickness_bins = Field(
         """
         Number of bins for by depth or by height analyses.
         """,
-        __default_value__=np.int(50))
+        __default_value__=50))
