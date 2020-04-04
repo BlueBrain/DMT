5c6777481e39ce98148c8a7cbc7e9a68dca3119e
Vishal Sood <vishal.chandra.sood@gmail.com>
2020-04-04 22:20:35 +0200

    clean up imports

--- neuro_dmt/utils/geometry/__init__.py
+++ neuro_dmt/utils/geometry/__init__.py
@@ -14,5 +14,28 @@
 # DMT source-code.  If not, see <https://www.gnu.org/licenses/>. 
 
 """
-Test network charts
+Generic, utilities to handle geomtries.
 """
+
+from dmt.tk.field import Field, lazyfield, WithFields
+
+class Interval(WithFields):
+    """
+    An `Interval` is two floats.
+    """
+    bottom = Field(
+        """
+        The bottom of the interval.
+        """)
+    top = Field(
+        """
+        The top of the interval,.
+        """)
+
+    @lazyfield
+    def thickness(self):
+        """
+        Thickness is the length of the this `Interval`.
+        """
+        return self.top - self.bottom
+
